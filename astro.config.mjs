cf0161601605e4051d0953c6dc2701a08ed59231
albinchristo04 <priyanka0101111@gmail.com>
2026-01-10 10:08:17 +0530

    2nd

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.rojadirectaenvivo.live',
-  // integrations: [sitemap()],
+  integrations: [sitemap()],
   output: 'static',
   trailingSlash: 'always',
   build: {
